67b752350dc8bd67a499e1ff6e6a7f41c470a91d
azhar982111 <azhar982111@gmail.com>
2022-09-29 00:14:41 +0530

    scrollcounter

--- useref-forms/src/App.jsx
+++ useref-forms/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css';
 import Form from './components/forms';
 import { Input } from './components/inputFocus';
+import { ScrollCounter } from './components/scrollCounter';
 import Counter from './components/Timer';
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <div className="App">
       {/* <Form/> */}
     {/* <Input/> */}
-    <Counter/>
+    {/* <Counter/> */}
+    <ScrollCounter/>
     </div>
   );
 }
